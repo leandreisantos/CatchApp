e3eae08060b4f727a7bc7c81b02ef2a2f44067b7
unknown <leandreisantos63@gmail.com>
2022-03-04 11:39:34 +0800

    first-commit

--- app/src/main/java/com/example/catchapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/catchapp/SplashScreenActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
@@ -17,11 +20,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         Handler handler = new Handler();
         handler.postDelayed(() -> {
-            Intent intent = new Intent(SplashScreenActivity.this,SigInActivity.class);
-            startActivity(intent);
+            if(user != null){
+                Intent intent = new Intent(SplashScreenActivity.this,QuestionActivity.class);
+                startActivity(intent);
+                finish();
+            }else{
+                Intent intent = new Intent(SplashScreenActivity.this,SigInActivity.class);
+                startActivity(intent);
+            }
         },3000);
     }
 }
